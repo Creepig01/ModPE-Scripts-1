cc403038c1a7fbcd90435b62ce66d731e816cfd2
Robert Montgomery <creepig01@gmail.com>
2013-10-14 11:15:53 -0400

    Smelting_Touch_With_IOS_Support.js

--- SmeltingTouch.js
+++ SmeltingTouch.js
@@ -8,31 +8,31 @@ function useItem(x,y,z,itemId,blockId){
         preventDefault();
     }else if(blockId==17){//Wood->Coal
 		setTile(x,y,z,0);
-		Player.addItemInventory(263,1);
+		addItemInventory(263,1);
 		preventDefault();
 	}else if(blockId==87){//Netherrack->Nether Brick
 		setTile(x,y,z,0);
-		Player.addItemInventory(405,1);
+		addItemInventory(405,1);
 		preventDefault();
 	}else if(blockId==56){//Diamond Ore->Diamond
 		setTile(x,y,z,1);
-		Player.addItemInventory(264,1);
+	        addItemInventory(264,1);
 		preventDefault();
 	}else if(blockId==15){//Iron Ore->Iron Ingot
 		setTile(x,y,z,1);
-		Player.addItemInventory(265,1);
+		addItemInventory(265,1);
 		preventDefault();
 	}else if(blockId==14){//Gold Ore->Gold Ingots
 		setTile(x,y,z,1);
-		Player.addItemInventory(266,1);
+		addItemInventory(266,1);
 		preventDefault();
 	}else if(blockId==16){//Coal Ore->Coal
 		setTile(x,y,z,1);
-		Player.addItemInventory(263,1);
+		addItemInventory(263,1);
 		preventDefault();
 	}else if(blockId==81){//Cactus->Dye
 		setTile(x,y,z,0);
-		Player.addItemInventory(351,1);
+		addItemInventory(351,1);
 		preventDefault();
 	}
 }
